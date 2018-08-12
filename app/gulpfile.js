09af2dce23903ed046abdf7308f25f56cbb20e99
Farid <farid@tgalimited.com>
2018-08-12 18:42:04 +0600

    initial commit

---
+++ app/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+
+gulp.task('styles', function(){
+    gulp.src('styles/scss/style.scss')
+        .pipe(sass())
+        .pipe(autoprefixer(
+            {browsers : ['last 2 versions']}
+        ))
+        .pipe(minifycss())
+        .pipe(gulp.dest('styles/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./styles/scss/*.scss', ['styles']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);
+
